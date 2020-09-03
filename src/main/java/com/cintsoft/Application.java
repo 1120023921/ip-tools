66d6ea968f3c451d7ac8c343f228ee8413636051
胡昊 <huhao9277@gmail.com>
2020-09-03 19:08:05 +0800

    虚拟目录配置

--- src/main/java/com/cintsoft/Application.java
+++ src/main/java/com/cintsoft/Application.java
@@ -11,7 +11,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
     }
 
